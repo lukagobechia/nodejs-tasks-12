00104894c1f24f3287e04472c788c2c29284394e
Luka Gobechia <lukagobechiaa@gmail.com>
2024-11-11 05:38:28 +0400

    added task 1

--- task1.js
+++ task1.js
@@ -1,19 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
-function folder(folderName, basePath) {
-  const folderPath = path.join(basePath, folderName);
+function folder(folderName) {
+  const folderPath = path.join(__dirname, folderName);
   console.log(folderPath);
 
-  try {
-    if (fs.existsSync(folderPath)) {
-      console.log(`The folder "${folderName}" exists.`);
-    } else {
-      console.log(`The folder "${folderName}" does not exist.`);
-    }
-  } catch (error) {
-    console.error(`An error occurred: ${error.message}`);
+  if (fs.existsSync(folderPath)) {
+    console.log('The folder exists.');
+  } else {
+    console.log('The folder does not exist.');
   }
 }
 
-folder("go",  __dirname);
+folder("go");
